2bfda4c6b98631d79a2e3d481914ec7c068b2d02
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-21 22:49:21 +0200

    upd: users.index route with paginate

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,25 @@
 
 use App\Models\Position;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return view('welcome');
 })->name('main');
 
-Route::get('/users', function () {
-  $users = User::with('position')->get();
+// Users
+Route::get('/users', function (Request $request) {
+  $users = User::with('position')->paginate(6);
 
+  if ($request->ajax()) {
+    return Blade::render('<x-user-items :users="$users" />', ['users' => $users]);
+  }
   return view('users.index', compact('users'));
 })->name('users.index');
 
+// Positions
 Route::get('/positions', function () {
   $positions = Position::withCount('users')->get();
 
