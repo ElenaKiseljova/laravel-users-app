85897958345eb18e18ca27f8fee553dde65c95c8
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-21 12:57:14 +0200

    add: phone, photo and position_id columns to Users table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
       'email_verified_at' => now(),
       'password' => static::$password ??= Hash::make('password'),
       'remember_token' => Str::random(10),
+      'phone' => fake()->phoneNumber(),
+      'photo' => 'https://picsum.photos/id/' . mt_rand(1, 59) . '/200',
     ];
   }
 
---
+++ database/migrations/2025_03_21_102711_add_additional_columns_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->string('phone')->after('name');
+      $table->string('photo')->after('phone');
+      $table->foreignId('position_id')->after('id')->constrained();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->dropColumn('phone');
+      $table->dropColumn('photo');
+
+      $table->dropForeign(['position_id']);
+      $table->dropColumn('position_id');
+    });
+  }
+};
