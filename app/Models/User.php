dcda44bc698285aedb6a11ca2fdc261c9aa49bf9
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-23 18:13:00 +0200

    add: makeDirectory method to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable
 {
@@ -52,5 +53,14 @@ class User extends Authenticatable
   public function position()
   {
     return $this->belongsTo(Position::class);
+  }
+
+  public static function makeDirectory()
+  {
+    $directory = 'users';
+
+    Storage::makeDirectory($directory);
+
+    return $directory;
   }
 }
