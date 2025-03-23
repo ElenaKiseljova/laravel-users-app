a119cb1d9e3d69361cceb6386c2b4d3bae883fe3
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-23 13:38:39 +0200

    upd: complete Users routes (without store and update logic)

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,41 @@ Route::get('/users', function (Request $request) {
 })->name('users.index');
 
 Route::get('/users/create', function () {
+  $user = new User();
+
   $positions = Position::get();
 
-  return view('users.create', compact('positions'));
+  return view('users.create', compact('user', 'positions'));
 })->name('users.create');
 
 Route::post('/users/store', function (CreateUserRequest $request) {
   dd($request);
+
+  return to_route('users.index')->with('message', 'User has been created successfully');
 })->name('users.store');
+
+Route::get('/users/{user}/edit', function (User $user) {
+  $positions = Position::get();
+
+  return view('users.edit', compact('user', 'positions'));
+})->name('users.edit');
+
+Route::put('/users/{user}', function (Request $request) {
+  dd($request);
+
+  return back()->with('message', 'User has been updated successfully');
+})->name('users.update');
+
+Route::delete('/users/{user}', function (User $user) {
+  $user->delete();
+
+  return back()->with('message', 'User has been removed successfully');
+})->name('users.destroy');
+
+Route::get('/users/{user}', function (User $user) {
+  return view('users.show', compact('user'));
+})->name('users.show');
+
 
 // Positions
 Route::get('/positions', function () {
